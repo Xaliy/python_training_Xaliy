3935e42a7b9b0700eb88339c1d9ad78f1e3f214a
galgavrilenko <ggalkka@gmail.com>
2023-01-07 00:47:31 +0300

    exeption def app

--- test_add_group.py
+++ test_add_group.py
@@ -1,43 +1,28 @@
 # -*- coding: utf-8 -*-
-# from selenium import webdriver
-from selenium.common.exceptions import NoSuchElementException
-from selenium.common.exceptions import NoAlertPresentException
-import unittest
+import pytest
 
 from application import Application
 from classes import Group
 
 
-class TestAddGroup(unittest.TestCase):
-    def setUp(self):
-        self.app = Application()
+@pytest.fixture
+def app(request):
+	fixture = Application()
+	request.addfinalizer(fixture.destroy)
+	return fixture
 
-    def is_element_present(self, how, what):
-        try: self.wd.find_element(by=how, value=what)
-        except NoSuchElementException as e: return False
-        return True
 
-    def is_alert_present(self):
-        try: self.wd.switch_to_alert()
-        except NoAlertPresentException as e: return False
-        return True
+def test_add_group(app):
+	app.login(username='admin', password='secret')
+	app.create_group(Group(name='new_group',
+						   header='хедер Группы',
+						   footer='футтер группы'))
+	app.logaut_website()
 
-    def test_add_group(self):
-        self.app.login(username='admin', password='secret')
-        self.app.create_group(Group(name='new_group',
-                                    header='хедер Группы',
-                                    footer='футтер группы'))
-        self.app.logaut_website()
 
-    def test_add_empty_group(self):
-        self.app.login(username="admin", password="secret")
-        self.app.create_group(Group(name="",
-                                    header="",
-                                    footer=""))
-        self.app.logaut_website()
-
-    def tearDown(self):
-        self.app.destroy()
-
-if __name__ == "__main__":
-    unittest.main()
+def test_add_empty_group(app):
+	app.login(username="admin", password="secret")
+	app.create_group(Group(name="",
+						   header="",
+						   footer=""))
+	app.logaut_website()
