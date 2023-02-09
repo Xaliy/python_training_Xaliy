ba20200eb567571d4592864e78862026c5f52117
galgavrilenko <ggalkka@gmail.com>
2023-02-10 01:14:40 +0300

    Передача параметров из командной строки ругается на __init__

--- conftest.py
+++ conftest.py
@@ -6,18 +6,22 @@ from fixture.application import Application
 
 fixture = None
 
+
 # @pytest.fixture(scope="session")
 @pytest.fixture
 def app(request):
     global fixture
 
     if fixture is None:
-        fixture = Application()
-    elif not fixture.is_valid():
-        fixture = Application()
+        browser = request.config.getoption("--browser")
+        fixture = Application(browser=browser)
+    else:
+        if not fixture.is_valid():
+            fixture = Application()
 
-    fixture.session.ensure_login(username='admin', password='secret')
+    fixture.session.ensure_login(username="admin", password="secret")
     return fixture
+
 
 # autouse=True - автом. использование метода
 @pytest.fixture(scope="session", autouse=True)
@@ -28,3 +32,11 @@ def stop(request):
 
     request.addfinalizer(fin)
     return fixture
+
+
+# добавление дополнительных параметров
+# в данном случае ввод значения из консоли
+def pytest_addoption(parser):
+    """функция добавления опции командной строки"""
+    parser.addoption("--browser", action="store",
+                     defaulf="firefox")
--- fixture/application.py
+++ fixture/application.py
@@ -1,4 +1,6 @@
-from selenium.webdriver.firefox.webdriver import WebDriver
+# from selenium.webdriver.firefox.webdriver import WebDriver
+from selenium import webdriver
+
 
 from fixture.contact import ContactHelper
 from fixture.group import GroupHelper
@@ -6,11 +8,18 @@ from fixture.session import SessionHelper
 
 
 class Application:
-    def __init__(self):
-        # self.wd = webdriver.Firefox()
-        # self.wd.implicitly_wait(100)
-        self.wd = WebDriver()
-        self.wd.implicitly_wait(3)  # в нашем случае это лишнее
+
+    def __init__(self, browser="firefox"):
+        # self.wd = WebDriver()
+        if browser == "firefox":
+            self.wd = webdriver.Firefox()
+        elif browser == "chrome":
+            self.wd = webdriver.Chrome()
+        elif browser == "edge":
+            self.wd = webdriver.Edge()
+        else:
+            raise ValueError(f"Unrecognized browser {browser}")
+        self.wd.implicitly_wait(3)  # в нашем случае это не лишнее
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
         self.contact = ContactHelper(self)
@@ -26,7 +35,7 @@ class Application:
         """Открыть форму авторизации приложения."""
         wd = self.wd
         # open home page
-        wd.get('http://localhost/addressbook/')
+        wd.get("http://localhost/addressbook/")
 
     def destroy(self):
         self.wd.quit()
